567d61e3dbf7e4ca6c0dba576079c66c6c1f18c0
Andrey Aksenov <agatissimo@gmail.com>
2025-05-05 12:12:12 +0300

    add base table page layout

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,12 +1,18 @@
 import { createRouter, createWebHistory } from 'vue-router'
 
 import HomeView from '@/views/HomeView.vue'
+import TableView from '@/views/TableView.vue'
 
 const routes = [
   {
     path: '/',
     name: 'Home',
     component: HomeView
+  },
+  {
+    path: '/table',
+    name: 'Table',
+    component: TableView
   }
 ]
 
---
+++ src/views/TableView.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+import TableHeader from '@/components/Table/TableHeader.vue'
+import TheToolbar from '@/components/Table/TheToolbar.vue'
+import TheFormula from '@/components/Table/TheFormula.vue'
+import TheTable from '@/components/Table/TheTable.vue'
+import TableFooter from '@/components/Table/TableFooter.vue'
+</script>
+
+<template>
+  <div class="bg-gray-1 grid h-screen w-screen grid-rows-[100px_28px_1fr_44px] transition-colors">
+    <div class="border-b-gray-3 flex flex-col justify-between border-b p-4 transition-colors">
+      <TableHeader />
+      <TheToolbar />
+    </div>
+
+    <TheFormula />
+    <TheTable />
+    <TableFooter />
+  </div>
+</template>
