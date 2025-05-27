089cbb405cdfb9da17f172ba640d850f746881a2
Andrey Aksenov <agatissimo@gmail.com>
2025-05-27 08:39:35 +0300

    refactor(types): update Sheet types

--- src/shared/types/sheet.ts
+++ src/shared/types/sheet.ts
@@ -5,11 +5,12 @@ export interface Sheet {
   tableId: string
   title: string
   order: number
+}
+
+export interface SheetData {
   rows: Record<string, Row>
   columns: Column[]
   cells: Record<string, Cell>
 }
 
 export type SheetDto = Pick<Sheet, 'sheetId' | 'tableId' | 'title' | 'order'>
-export type SheetMeta = Pick<Sheet, 'sheetId' | 'title' | 'order'>
-export type SheetData = Pick<Sheet, 'rows' | 'columns' | 'cells'>
