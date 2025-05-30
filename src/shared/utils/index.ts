b3920f38fb40a7b15b1364c34f59fc99c3f1dccd
Andrey Aksenov <agatissimo@gmail.com>
2025-05-30 11:30:35 +0300

    refactor(utils): update factories for Table and Sheet

--- src/shared/utils/index.ts
+++ src/shared/utils/index.ts
@@ -1,16 +1,28 @@
 import { formatTimestampToStringDate } from './formatTimestampToStringDate'
+import { numberToColumnLetter } from './numberToColumnLetter'
 import { setHtmlDark } from './setHtmlDark'
 
-import { createTableObject, fromTableDto, toTableDto } from './tableFactory'
-import { createSheetObject, fromSheetDto, toSheetDto } from './sheetFactory'
+import { generateTable, fromTableDto, toTableDto } from './tableFactory'
+import { generateSheet, fromSheetDto, toSheetDto } from './sheetFactory'
+import { generateSheetData } from './sheetDataFactory'
+import { generateColumns, transformColumnsFromArray } from './columnFactory'
+import { generateRows, transformRowsFromArray } from './rowFactory'
+import { generateCells } from './cellFactory'
 
 export {
   formatTimestampToStringDate,
+  numberToColumnLetter,
   setHtmlDark,
-  createTableObject,
+  generateTable,
   fromTableDto,
   toTableDto,
-  createSheetObject,
+  generateSheet,
   fromSheetDto,
-  toSheetDto
+  toSheetDto,
+  generateSheetData,
+  generateColumns,
+  transformColumnsFromArray,
+  generateRows,
+  transformRowsFromArray,
+  generateCells
 }
